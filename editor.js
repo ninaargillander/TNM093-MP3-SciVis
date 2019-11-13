8f12910e32e394bbe95341d3fab2fe0013501c9b
Nina Argillander <nina@argillander.se>
2019-11-13 17:00:14 +0100

    Reworked to a class

--- editor.js
+++ editor.js
@@ -1,33 +1,95 @@
-// Handles the transfer function editor
+class Editor {
+  constructor(
+    width,
+    height,
+    margins,
+    previewHeight,
+    previewTopPadding,
+    nodeManager
+  ) {
+    this.width = width;
+    this.height = height;
+    this.margins = margins;
+    this.previewHeight = previewHeight;
+    this.previewTopPadding = previewTopPadding;
+    this.nodeManager = nodeManager;
 
-// Document selections
-let div = d3.select('#transferFunctionEditor');
-let svg = div.append('svg');
-let defs = svg.append('defs');
+    // Width and height of editor and preview combined
+    this.innerWidth = width - margins.left - margins.right;
+    this.innerHeight = height - margins.top - margins.bottom;
 
-// Editor parameters
-const width = 250;
-const height = 50;
-const margins = {top: 10, bottom: 10, left: 10, right: 10};
+    // Document selections
+    this.div = d3.select('#transferFunctionEditor');
+    this.svg = this.div.append('svg');
+    this.defs = this.svg.append('defs');
+    this.previewGradient = this.defs
+      .append('linearGradient')
+      .attr('id', 'previewGradient');
 
-// Transfer function preview window
-const topPadding = 10;
-const previewHeight = 50;
+    // Set up the svg groups for the editor and preview
+    this.svg.attr('width', width).attr('height', height);
 
-// Set up the svg groups for the editor and preview
-// TODO
+    // Selection handle for editor rectangle group
+    this.editorGroup = this.svg
+      .append('g')
+      .attr('transform', `translate(${margins.left}, ${margins.top})`);
 
-// Temporary initialization of global class instance
-let TFGen = new TransferFunctionGenerator(256, width);
+    // Selection handle for preview rectangle group
+    this.previewGroup = this.svg
+      .append('g')
+      .attr(
+        'transform',
+        `translate(${margins.left}, ${margins.top +
+          this.innerHeight -
+          this.previewHeight})`
+      );
 
-// Try adding a node
-TFGen.addNode(67, 12, 19, 110, 140)
-TFGen.addNode(17, 32, 29, 12, 240)
-TFGen.addNode(69, 15, 99, 0, 42)
-TFGen.addNode(37, 62, 109, 3, 80)
-TFGen.addNode(167, 19, 69, 10, 10);
-TFGen.addNode(width, 19, 69, 10, 10);
+    // Draw editor
+    this.draw();
+  }
 
+  draw() {
+    // Start by generating/updating the gradient
+    this.generateGradient();
 
-// Try asking for the transfer function
-TFGen.getTransferFunction()
+    // Add a test rect
+    this.editorGroup
+      .append('rect')
+      .attr('width', this.innerWidth)
+      .attr(
+        'height',
+        this.innerHeight - this.previewHeight - this.previewTopPadding
+      )
+      .attr('fill', 'red');
+
+    // Add a test rect
+    this.previewGroup
+      .append('rect')
+      .attr('width', this.innerWidth)
+      .attr('height', this.previewHeight)
+      .attr('fill', 'url(#previewGradient)');
+
+    // Draw editor 'background'
+    // this.svg
+    //   .append('rect')
+    //   .attr('width', this.width)
+    //   .attr('height', this.height)
+    //   .attr('fill', 'black')
+    //   .lower();
+  }
+
+  generateGradient() {
+    // Get all nodes
+    const nodes = this.nodeManager.getNodes();
+
+    this.previewGradient
+      .selectAll('stop')
+      .data(nodes)
+      .join(enter => {
+        enter
+          .append('stop')
+          .attr('stop-color', node => node.getCSSColor())
+          .attr('offset', node => node.getOffsetPercentage(this.innerWidth));
+      });
+  }
+}
