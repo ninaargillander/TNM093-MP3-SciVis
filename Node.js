f076101ac2c608d6d2fa35afa8120ca4222ffbf2
Nina Argillander <nina@argillander.se>
2019-11-13 17:01:03 +0100

    Added helper functions for linear gradients

--- Node.js
+++ Node.js
@@ -1,20 +1,28 @@
 class Node {
-	constructor(x, y, r, g, b) {
-		this.x = x;
-		this.y = y;
-		this.r = r;
-		this.g = g;
-		this.b = b;
-	}
+  constructor(x, y, r, g, b) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.g = g;
+    this.b = b;
+  }
 
-	setXY(x, y) {
-		this.x = x;
-		this.y = y;
-	}
+  setXY(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
-	setRGB(r, g, b) {
-		this.r = r;
-		this.g = g;
-		this.b = b;
-	}
+  setRGB(r, g, b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+  }
+
+  getCSSColor() {
+    return d3.rgb(this.r, this.g, this.b, this.y);
+  }
+
+  getOffsetPercentage(width) {
+    return `${(100 * this.x) / width}%`;
+  }
 }
